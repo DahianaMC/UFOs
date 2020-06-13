c88decd8bbcd0f79fd953b9a5b6023dcd3df84fe
DahianaMC <dahiana_mc@hotmail.com>
2020-06-13 09:34:37 -0500

    updating static folder

--- static/js/app.js
+++ static/js/app.js
@@ -4,26 +4,25 @@ const tableData = data;
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
 
-// Build table to display all of the UFO sightings
 function buildTable(data) {
-    // First, clear out any existing data
-    tbody.html("");
-  }
+  // First, clear out any existing data
+  tbody.html("");
 
-// Next, loop through each object in the data
-// and append a row and cells for each value in the row
-data.forEach((daraRow) => {
+  // Next, loop through each object in the data
+  // and append a row and cells for each value in the row
+  data.forEach((dataRow) => {
     // Append a row to the table body
     let row = tbody.append("tr");
 
     // Loop through each field in the dataRow and add
     // each value as a table cell (td)
-    Object.Values(dataRow).forEach((val) => {
-        let cell = row.append("td");
-        cell.text(val);
-    }
+    Object.values(dataRow).forEach((val) => {
+      let cell = row.append("td");
+      cell.text(val);
+      }
     );
-});
+  });
+}
 
 // Creating a function to filter the data by date
 function handleClick() {
